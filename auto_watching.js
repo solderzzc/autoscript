c8fb87c2af5cc3488c677921faad7ebd776f5051
Simba Zhang <solderzzc@gmail.com>
2017-01-12 15:11:41 -0800

    init

---
+++ auto_watching.js
@@ -0,0 +1,47 @@
+var Nightmare = require('nightmare');       
+
+function init(){
+
+var nightmare = Nightmare({ show: true, 
+  webPreferences: {
+    partition: 'persist: testing'
+}});
+
+Nightmare.action('clearCache',
+  function(name, options, parent, win, renderer, done) {
+    parent.respondTo('clearCache', function(done) {
+      //win.webContents.session.clearCache(done);
+    });
+    done();
+  },
+  function(done) {
+    this.child.call('clearCache', done);
+  });
+
+nightmare
+  .goto('http://demo-cloud.apps.58dahuo.com/')
+  .wait(10000)
+  .wait('#playbtn')
+  .click('#playbtn')
+  .evaluate(function () {
+                return 'ok';
+            })
+  .wait(29000)
+  .end()
+  .then(function (result) {
+    console.log(result);
+    setTimeout(function(){
+      init()
+    },3000)
+  })
+  .catch(function (error) {
+    console.error('Search failed:', error);
+    setTimeout(function(){
+      init()
+    },3000)
+  });
+}
+init()
+init()
+init()
+init()
